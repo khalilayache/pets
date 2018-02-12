859ad76d9a6f383a43e09638cdc8d576de86928b
khalilayache <khalilayache@gmail.com>
2018-02-11 22:44:46 -0200

    Class 4 Lesson 11

--- app/src/main/java/com/khalilayache/pets/EditorActivity.kt
+++ app/src/main/java/com/khalilayache/pets/EditorActivity.kt
@@ -1,8 +1,12 @@
 package com.khalilayache.pets
 
+import android.app.LoaderManager
 import android.content.ContentValues
 import android.content.Context
+import android.content.CursorLoader
 import android.content.Intent
+import android.content.Loader
+import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.NavUtils
@@ -10,15 +14,18 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import com.khalilayache.pets.data.PetContract
+import com.khalilayache.pets.data.PetContract.PetEntry.COLUMN_BREED
+import com.khalilayache.pets.data.PetContract.PetEntry.COLUMN_GENDER
+import com.khalilayache.pets.data.PetContract.PetEntry.COLUMN_NAME
+import com.khalilayache.pets.data.PetContract.PetEntry.COLUMN_WEIGHT
 import com.khalilayache.pets.data.PetContract.PetEntry.PET_CONTENT_URI
 import kotlinx.android.synthetic.main.activity_editor.editBreed
 import kotlinx.android.synthetic.main.activity_editor.editName
 import kotlinx.android.synthetic.main.activity_editor.editWeight
 import kotlinx.android.synthetic.main.activity_editor.spinnerGender
 
-class EditorActivity : AppCompatActivity() {
+class EditorActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
 
   companion object {
     fun createIntent(context: Context) = Intent(context, EditorActivity::class.java)
@@ -30,14 +37,21 @@ class EditorActivity : AppCompatActivity() {
     }
   }
 
+  private val EDITOR_LOADER = 1001
+
+  private var CURRENT_URI: Uri? = null
+
+  private val petCursorAdapter by lazy { PetCursorAdapter(this@EditorActivity, null) }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_editor)
 
+
+    initActivity()
     initSpinner()
     initToolbar()
   }
-
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
     menuInflater.inflate(R.menu.menu_editor, menu)
@@ -61,18 +75,53 @@ class EditorActivity : AppCompatActivity() {
     return super.onOptionsItemSelected(item)
   }
 
+  override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+
+
+    return CursorLoader(this,
+        CURRENT_URI,
+        null,
+        null,
+        null,
+        null)
+  }
+
+  override fun onLoadFinished(loader: Loader<Cursor>?, cursor: Cursor?) {
+    if (cursor == null || cursor.count < 1) {
+      return
+    }
+
+    if (cursor.moveToFirst()) {
+      editBreed.setText(cursor.getString(cursor.getColumnIndex(COLUMN_BREED)))
+      editName.setText(cursor.getString(cursor.getColumnIndex(COLUMN_NAME)))
+      editWeight.setText(cursor.getInt(cursor.getColumnIndex(COLUMN_WEIGHT)).toString())
+      spinnerGender.setSelection(cursor.getInt(cursor.getColumnIndex(COLUMN_GENDER)))
+    }
+  }
+
+  override fun onLoaderReset(loader: android.content.Loader<Cursor>?) {
+    editBreed.setText("")
+    editName.setText("")
+    editWeight.setText("")
+    spinnerGender.setSelection(0)
+  }
+
+  private fun initLoader() {
+    loaderManager.initLoader<Cursor>(EDITOR_LOADER, null, this)
+  }
+
   private fun saveNewPet() {
     val name = editName.text.toString().trim()
     val breed = editBreed.text.toString().trim()
     val weight = editWeight.text.toString().trim().toInt()
     val gender = spinnerGender.selectedItem.toString()
 
-    val savedId = contentResolver.insert(PET_CONTENT_URI, getContentValues(Pet(name, breed, gender, weight)))
-    if (savedId != Uri.EMPTY) {
-      Toast.makeText(this@EditorActivity, "Pet saved with id: $savedId", Toast.LENGTH_SHORT).show()
+    if (CURRENT_URI == null) {
+      contentResolver.insert(PET_CONTENT_URI, getContentValues(Pet(name, breed, gender, weight)))
     } else {
-      Toast.makeText(this@EditorActivity, "Error with saving pet", Toast.LENGTH_SHORT).show()
+      contentResolver.update(CURRENT_URI, getContentValues(Pet(name, breed, gender, weight)), null, null)
     }
+
   }
 
   private fun getContentValues(pet: Pet): ContentValues {
@@ -104,12 +153,18 @@ class EditorActivity : AppCompatActivity() {
   }
 
   private fun initToolbar() {
-    if (intent.data != null) {
+    if (CURRENT_URI != null) {
       setTitle(R.string.edit_pet)
+
+      initLoader()
     } else {
       setTitle(R.string.add_pet)
     }
 
+  }
+
+  private fun initActivity() {
+    CURRENT_URI = intent.data
   }
 
 }
